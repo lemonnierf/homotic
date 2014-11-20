d81fd89d6176a1ee58074c5a7af1dd260bc4f8d4
lemonnierf <frederic@le-monnier.net>
2014-11-20 22:06:17 +0000

    Read

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var gpio = require("pi-gpio");
 
-gpio.open(7, "output", function(err) {     // Open pin 16 for output
-gpio.read(7, function(err, value) {
+gpio.open(4, "output", function(err) {     // Open pin 16 for output
+gpio.read(4, function(err, value) {
     if(err) throw err;
     console.log(value); // The current state of the pin
     });
